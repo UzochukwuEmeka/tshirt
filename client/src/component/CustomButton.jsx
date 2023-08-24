817a8f1562349816f34c7729d41412168b764247
unknown <uzochukwuemeka516@gmail.com>
2023-08-23 18:19:59 -0700

    first commit

---
+++ client/src/component/CustomButton.jsx
@@ -0,0 +1,31 @@
+
+import state from '../store'
+import { useSnapshot } from 'valtio'
+import React from 'react'
+import { getContrastingColor } from '../config/helpers'
+
+const CustomButton = ({ type, handleClick, title, customStyle }) => {
+    const snap = useSnapshot(state)
+    const generateButton = (type) => {
+        if (type === 'filled') {
+            return {
+                background: snap.color,
+                color: getContrastingColor(snap.color)
+            }
+
+        } else if (type === "outline") {
+            return {
+                borderWidth: '1px',
+                borderColor: snap.color,
+                color: snap.color
+            }
+        }
+
+    }
+
+    return (
+        <button className={`px-2 py-1.5 flex-1 rounded-md ${customStyle}`} style={generateButton(type)} onClick={handleClick} >{title}</button>
+    )
+}
+
+export default CustomButton
